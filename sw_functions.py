1bfbdf64bc12ba4e9ea06b28051006f0fc030a85
Kenneth Allen <kencubed@gmail.com>
2023-01-19 22:13:29 -0500

    updated notebook and functions

--- sw_functions.py
+++ sw_functions.py
@@ -10,11 +10,11 @@ def sub_df(df, column_values, column_name):
 def load_main_monsters():
     #with open('cardinfo.php.json') as file_path:
     with open('cardinfo.json') as file_path:
-        cards_json = json.load(file_path)
-    all_cards = pd.DataFrame(cards_json['data'])
-    all_cards = all_cards.rename(columns={'type': 'card_type','race':'type'})
+        json_all_cards = json.load(file_path)
+    df_all_cards = pd.DataFrame(json_all_cards['data'])
+    df_all_cards = df_all_cards.rename(columns={'type': 'category','race':'type'})
 
-    main_monster_card_types = ['Effect Monster',
+    main_monster_card_category = ['Effect Monster',
                                 'Normal Monster',
                                 'Flip Effect Monster',
                                 'Union Effect Monster',
@@ -30,18 +30,18 @@ def load_main_monsters():
                                 'Pendulum Tuner Effect Monster',
                                 'Pendulum Effect Ritual Monster',
                                 'Pendulum Flip Effect Monster']
-    main_monsters = sub_df(all_cards, main_monster_card_types, 'card_type').reset_index(drop=True)
-    main_monsters = main_monsters[['name','type','attribute','level','atk','def']] #keep only relevant columns
-    return main_monsters
+    df_main_monsters = sub_df(df_all_cards, main_monster_card_category, 'category').reset_index(drop=True)
+    df_main_monsters = df_main_monsters[['name','type','attribute','level','atk','def']] #keep only relevant columns
+    return df_main_monsters
 
-def create_sw_adjacency_matrix(deck_mosters):
+def create_sw_adjacency_matrix(df_cards):
     #creates adjacency matrix corresponding to Small World connections
     #two cards are considered adjacent if they have exactly one type, attribute, level, atk, or def in common
-    deck_monsters_array = deck_mosters.to_numpy()
-    num_cards = len(deck_mosters)
+    array_cards = df_cards.to_numpy()
+    num_cards = len(df_cards)
     adjacency_matrix = np.zeros((num_cards,num_cards))
     for i in range(num_cards):
-        card_similarities = deck_monsters_array==deck_monsters_array[i]
+        card_similarities = array_cards==array_cards[i]
         similarity_measure = card_similarities.astype(int).sum(axis=1)
         adjacency_matrix[:,i] = (similarity_measure==1).astype(int) #indicates where there is exactly one similarity
     return adjacency_matrix
