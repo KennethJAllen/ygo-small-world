9bf942a9d4116390e65c7e6eb50e9791e96607d8
Kenneth Allen <kencubed@gmail.com>
2023-02-02 21:55:03 -0500

    updated small world functions and small world image generation notebook

--- small_world_functions.py
+++ small_world_functions.py
@@ -1,6 +1,12 @@
 import json
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import networkx as nx
+import requests
+from PIL import Image
+from io import BytesIO
 
 def sub_df(df, column_values, column_name):
     #creates subset of dataframe consisting of rows with column_values in column
@@ -42,7 +48,7 @@ def monster_names_to_df(card_names):
     df_cards = sub_df(MAIN_MONSTERS, card_names, 'name')
     return df_cards
 
-#### READING YDK FILES ####
+#### READ YDK FILES ####
 
 def ydk_to_card_ids(ydk_file):
     #convers a ydk file to card ids
@@ -66,7 +72,7 @@ def ydk_to_monster_names(ydk_file):
     monster_names = df_monsters['name'].tolist()
     return monster_names
 
-#### ADJACENCY MATRICES GENERATION ####
+#### ADJACENCY MATRIX GENERATION ####
 
 def df_to_adjacency_matrix(df_cards, squared=False):
     #creates adjacency array corresponding to Small World connections
@@ -121,7 +127,6 @@ def find_best_bridges(deck_monster_names, required_target_names=[]):
     if len(df_bridges)==0:
         print('There are no monsters that bridge all required targets.')
         return
-
     #subset of adjacency matrix corresponding to (deck monsters) by (monsters with connections to the required cards)
     bridge_matrix = SW_ADJACENCY_MATRIX[deck_indices,:][:,required_bridge_indices]
 
@@ -139,4 +144,161 @@ def find_best_bridges_from_ydk(ydk_file):
     df_bridges = find_best_bridges(deck_monster_names)
     return df_bridges
 
-#### IMAGE GENERATION ####
+#### GET CARD IMAGES ####
+
+def names_to_image_urls(card_names):
+    with open('cardinfo.json') as file_path:
+        json_all_cards = json.load(file_path)
+    df_all_cards = pd.DataFrame(json_all_cards['data']) #dataframe of all cards to get image links
+
+    df_deck_images = sub_df(df_all_cards,card_names,'name')
+    df_deck_images['card_images'] = df_deck_images['card_images'].apply(lambda x: x[0]['image_url_cropped'])
+    urls = df_deck_images['card_images'].tolist()
+    return urls
+
+def load_image(url): 
+    res = requests.get(url)
+    imgage = np.array(Image.open(BytesIO(res.content)))
+    return imgage
+
+def load_images(urls):
+    images = []
+    for url in urls:
+        image = load_image(url)
+        images.append(image)
+    return images
+
+CARD_SIZE = 624
+MAX_PIXEL_BRIGHTNESS = 255
+
+def normalize_images(images):
+    normalized_images = []
+    for image in images:
+        image_length = image.shape[0]
+        image_width = image.shape[1]
+        normalized_image = np.ones([CARD_SIZE,CARD_SIZE,3])*MAX_PIXEL_BRIGHTNESS
+        #covering cases when image is too small
+        if image_length<CARD_SIZE and image_width<CARD_SIZE: #case when length & width are too small
+            normalized_image[:image_length,:image_width,:] = image
+        elif image_length<CARD_SIZE: #case when only length is too small
+            normalized_image[:image_length,:,:] = image[:,:CARD_SIZE,:]
+        elif image_width<CARD_SIZE: #case when only width is too small
+            normalized_image[:,:image_width,:] = image[:CARD_SIZE,:,:]
+        else: #case when image is same size or too big
+            normalized_image = image[:CARD_SIZE,:CARD_SIZE,:]
+        normalized_image = normalized_image.astype(np.uint8)
+        normalized_images.append(normalized_image)
+    return normalized_images
+
+def names_to_images(card_names):
+    urls = names_to_image_urls(card_names)
+    images = load_images(urls)
+    normalized_images = normalize_images(images)
+    return normalized_images
+
+#### CREATE GRAPH IMAGE ####
+
+def matrix_to_graph_image(connection_matrix, card_images):
+    G = nx.from_numpy_array(connection_matrix)
+    for i in range(len(card_images)):
+        G.nodes[i]['image'] = card_images[i] #asigns image to each node
+
+    pos=nx.circular_layout(G)
+
+    fig=plt.figure(figsize=(5,5))
+    ax=plt.subplot(111)
+    ax.set_aspect('equal')
+    nx.draw_networkx_edges(G,pos,ax=ax, width=1.3), 
+
+    plt.xlim(-1,1)
+    plt.ylim(-1,1)
+
+    trans=ax.transData.transform
+    trans2=fig.transFigure.inverted().transform
+
+    num_cards = len(card_images)
+    piesize = -0.003*num_cards+0.15 #image size is a linear function of the number of cards
+    p2=piesize/2.0
+    for n in G:
+        xx,yy=trans(pos[n]) #figure coordinates
+        xa,ya=trans2((xx,yy)) #axes coordinates
+        a = plt.axes([xa-p2,ya-p2, piesize, piesize])
+        a.set_aspect('equal')
+        a.imshow(G.nodes[n]['image'])
+        a.axis('off')
+    ax.axis('off')
+    plt.savefig('images\small-wolrd-graph.png')
+    plt.show()
+
+def names_to_graph_image(card_names):
+    card_images = names_to_images(card_names)
+    df_deck = monster_names_to_df(card_names).reset_index(drop=True)
+    connection_matrix = df_to_adjacency_matrix(df_deck)
+    matrix_to_graph_image(connection_matrix, card_images)
+
+def ydk_to_graph_image(ydk_file):
+    card_names = ydk_to_monster_names(ydk_file)
+    names_to_graph_image(card_names)
+
+#### CREATE MATRIX IMAGE ####
+
+def matrix_to_image(connection_matrix, card_images, squared=False, highlighted_columns=[]):
+    num_cards = len(card_images)
+
+    vertical_cards = np.concatenate(card_images, axis=1) #concatenated images horizontally
+    horizontal_cards = np.concatenate(card_images, axis=0) #concatenated images vertically
+    image_size = CARD_SIZE*(num_cards+1)
+    matrix_subimage_size = CARD_SIZE*num_cards
+
+    full_image = np.ones((image_size,image_size,3))*MAX_PIXEL_BRIGHTNESS
+
+    #card images
+    full_image[CARD_SIZE:,0:CARD_SIZE,:] = horizontal_cards
+    full_image[0:CARD_SIZE,CARD_SIZE:,:] = vertical_cards
+
+    matrix_subimage = (np.ones((matrix_subimage_size,matrix_subimage_size,3))*MAX_PIXEL_BRIGHTNESS)
+
+    matrix_maximum = np.max(connection_matrix)
+    if highlighted_columns != []:
+        highlighted_maximum = np.max(connection_matrix[:,highlighted_columns])
+
+    #color in cells
+    for i in range(num_cards):
+        for j in range(num_cards):
+            matrix_entry = connection_matrix[i,j]
+            if matrix_entry>0:
+                if j in highlighted_columns:
+                    #highlighted cell color
+                    red_highlight_max = 255
+                    green_highlight_max = 220
+                    blue_highlight_max = 220
+                    matrix_subimage[i*CARD_SIZE:(i+1)*CARD_SIZE,j*CARD_SIZE:(j+1)*CARD_SIZE,0] = red_highlight_max
+                    matrix_subimage[i*CARD_SIZE:(i+1)*CARD_SIZE,j*CARD_SIZE:(j+1)*CARD_SIZE,1] = green_highlight_max*(1-matrix_entry/highlighted_maximum)
+                    matrix_subimage[i*CARD_SIZE:(i+1)*CARD_SIZE,j*CARD_SIZE:(j+1)*CARD_SIZE,2] = blue_highlight_max*(1-matrix_entry/highlighted_maximum)
+                else:
+                    #greyscale cell color
+                    cell_brightness_max = 220
+                    matrix_subimage[i*CARD_SIZE:(i+1)*CARD_SIZE,j*CARD_SIZE:(j+1)*CARD_SIZE,:] = cell_brightness_max*(1-matrix_entry/matrix_maximum)
+    full_image[CARD_SIZE:,CARD_SIZE:,:] = matrix_subimage
+
+    full_image = full_image.astype(np.uint8)
+
+    #create figure
+    fig = plt.imshow(full_image)
+    ax = plt.subplot(111)
+    ax.axis('off')
+    if squared==False:
+        plt.savefig('images\small-world-matrix.png', dpi=128.5, bbox_inches='tight')
+    else:
+        plt.savefig('images\small-world-matrix-squared.png', dpi=128.5, bbox_inches='tight')
+    plt.show()
+
+def names_to_matrix_image(card_names, squared=False):
+    card_images = names_to_images(card_names)
+    df_deck = monster_names_to_df(card_names).reset_index(drop=True)
+    connection_matrix = df_to_adjacency_matrix(df_deck, squared=squared)
+    matrix_to_image(connection_matrix, card_images, squared=squared)
+
+def ydk_to_matrix_image(ydk_file, squared=False):
+    card_names = ydk_to_monster_names(ydk_file)
+    names_to_matrix_image(card_names, squared=squared)
